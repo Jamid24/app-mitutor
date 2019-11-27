fd4fd32ce69b2ecfef95ac6dfbbeb14080b3d7b3
Jamid24 <rogers_jamid@hotmail.com>
2019-11-27 02:37:12 -0500

    ajustes creacion agenda

--- dist/js/main-calendar.js
+++ dist/js/main-calendar.js
@@ -23,8 +23,9 @@ $(function () {
       editable  : true,
       droppable : true // this allows things to be dropped onto the calendar !!!
       ,dateClick: function(info) {
-          getMatters();
-        console.log(info);
+        $('#form-tutorial')[0].reset();
+        getMatters(info.dateStr);
+        console.log(info.dateStr);
       }
       , eventClick: function(info) {
         alert('Event: ' + info.event.title);
@@ -34,6 +35,22 @@ $(function () {
 
     calendar.render();
     // $('#calendar').fullCalendar()
+
+    $("#tutorial-city").change(function () {
+      getOptionsSelect("#tutorial-sede", 2, this.value);
+    });
+
+    $("#tutorial-sede").change(function () {
+      getOptionsSelect("#tutorial-ubication", 3, this.value);
+    });
+
+    $('#form-tutorial').on('submit', function (e) {
+      e.preventDefault();
+      alert('sss');
+      createTutorial(calendar);
+    });
+
+/*
     var myEvent = {
         id:100,
         title:"Calculo Diferencial",
@@ -41,11 +58,11 @@ $(function () {
         start: new Date(),
         end: new Date()
       };
-      calendar.addEvent( myEvent );
+      calendar.addEvent( myEvent );*/
   })
 
 
-  function getMatters(){
+  function getMatters(dateSelected){
     $.ajax({
         url: urlWs+"TutorialController.php?action=1",
         data:JSON.stringify( {
@@ -62,6 +79,7 @@ $(function () {
             );
          }else{
            $('#modalTutoria').modal('show');
+           $('#tutorial-date').val(dateSelected);
            console.log(data.data);
            $("#tutorial-matter").html('');
            data.data.forEach(function(item) {
@@ -73,4 +91,76 @@ $(function () {
              console.log( "La solicitud a fallado: " +  textStatus);
          }
     });
+  }
+
+  function getOptionsSelect(select, action, value){
+    $.ajax({
+        url: urlWs+"TutorialController.php?action="+action,
+        data:JSON.stringify( {
+          "datum" : value
+        }),
+        type: "POST",
+        dataType: "json",
+    }).done(function( data, textStatus, jqXHR ) {
+         if ( data.status !==200 ) {
+           console.log(data);
+         }else{
+           $(select).html('');
+           data.data.forEach(function(item) {
+             $(select).append('<option value="'+item.value_name+'">'+item.text_name+'</option>');
+          });
+         }
+     }).fail(function( jqXHR, textStatus, errorThrown ) {
+         if ( console && console.log ) {
+             console.log( "La solicitud a fallado: " +  textStatus);
+         }
+    });
+  }
+
+
+  function createTutorial(calendar){
+    $.ajax({
+        url: urlWs+"TutorialController.php?action=4",
+        data:JSON.stringify( {
+          "idUser" : sessionStorage.getItem('ID'),
+          "idMatter" : $('#tutorial-matter').val(),
+          "idUbication" : $('#tutorial-ubication').val(),
+          "dateTutorial" : $('#tutorial-date').val(),
+          "hourTutorial" : $('#tutorial-hour').val(),
+          "quota" : $('#tutorial-quota').val()
+        }),
+        type: "POST",
+        dataType: "json",
+    }).done(function( data, textStatus, jqXHR ) {
+         if ( data.status !==200 ) {
+           Swal.fire(
+              'Lo sentimos!',
+              data.message,
+              'warning'
+            );
+         }else{
+           Swal.fire(
+              'Operación exitosa!',
+              data.message,
+              'success'
+            );
+            addEventCalendar(calendar, data.data.id_tutorial, $('#tutorial-matter :selected').text(), $('#tutorial-date').val(), $('#tutorial-date').val());
+         }
+         $('#modalTutoria').modal('hide');
+     }).fail(function( jqXHR, textStatus, errorThrown ) {
+         if ( console && console.log ) {
+             console.log( "La solicitud a fallado: " +  textStatus);
+         }
+    });
+  }
+
+  function addEventCalendar(calendar, id, text, dateIni, dateFin){
+    var myEvent = {
+        id:id,
+        title:text,
+        allDay: true,
+        start: dateIni,
+        end: dateFin
+      };
+      calendar.addEvent( myEvent );
   }
